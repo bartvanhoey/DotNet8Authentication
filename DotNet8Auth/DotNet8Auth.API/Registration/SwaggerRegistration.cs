83797ae30cbf414cc0473e7282a134ba57553899
ekapituro <ekapituro@gmail.com>
2023-12-20 15:25:32 +0100

    Swagger registration moved to its own file

---
+++ DotNet8Auth/DotNet8Auth.API/Registration/SwaggerRegistration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace DotNet8Auth.API.Registration;
+
+public static class SwaggerRegistration
+{
+    public static void RegisterSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter 'Bearer [access-token] in the Value input field'",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            var scheme = new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement { { scheme, Array.Empty<string>() } });
+        });
+
+    }
+}
