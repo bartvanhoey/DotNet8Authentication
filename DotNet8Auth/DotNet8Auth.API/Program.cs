1813989876ff561820cbf90144e2065fac8c9e0d
ekapituro <ekapituro@gmail.com>
2023-12-20 15:25:59 +0100

    Remove code in comment

--- DotNet8Auth/DotNet8Auth.API/Program.cs
+++ DotNet8Auth/DotNet8Auth.API/Program.cs
@@ -36,27 +36,6 @@ try
     
     builder.RegisterSwagger();
 
-    // builder.Services.AddSwaggerGen(options =>
-    // {
-    //     options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    //     {
-    //         In = ParameterLocation.Header,
-    //         Description = "Please enter 'Bearer [access-token] in the Value input field'",
-    //         Name = "Authorization",
-    //         Type = SecuritySchemeType.ApiKey
-    //     });
-    //
-    //     var scheme = new OpenApiSecurityScheme
-    //     {
-    //         Reference = new OpenApiReference
-    //         {
-    //             Type = ReferenceType.SecurityScheme,
-    //             Id = "Bearer"
-    //         }
-    //     };
-    //     options.AddSecurityRequirement(new OpenApiSecurityRequirement { { scheme, Array.Empty<string>() } });
-    // });
-
     // Setup DbContext
     builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
